22a8c82d33ca3928957d140db039f13a1a11fdfa
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-08-16 20:26:24 +0600

    varent animation update

--- src/components/animation3.tsx
+++ src/components/animation3.tsx
@@ -1,29 +1,49 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 
 const Animation3 = () => {
   const boxVariant = {
-    lion: {
-      x: 100,
-      scale: 1.5,
-      backgroundColor: "yellow",
+    hidden: {
+      x: "-100vw",
     },
-    elephant: {
-      x: 1000,
-      screen: 0.3,
+    visible: {
+      x: 0,
+      transition: {
+        delay: 0.5,
+        when: "beforeChildren",
+        staggerChildren: 0.2,
+      },
     },
   };
+
+  const listVariant = {
+    hidden: {
+      x: -10,
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+    },
+  };
+
   return (
     <div>
       <h3 className="text-2xl font-extrabold">Animation-3 (Variants)</h3>
       <motion.div
-        className="bg-sky-400 p-20 w-20 h-20 rounded-xl cursor-pointer"
+        className="max-w-44 bg-sky-400 p-20 rounded-xl cursor-pointer flex flex-col items-center gap-3"
         variants={boxVariant}
-        initial="lion"
-        animate="elephant"
-      ></motion.div>
+        animate="visible"
+        initial="hidden"
+      >
+        {[1, 2, 3].map((box, i) => (
+          <motion.div key={i} className="p-2 bg-white" variants={listVariant}>
+            {box}
+          </motion.div>
+        ))}
+      </motion.div>
     </div>
   );
 };
